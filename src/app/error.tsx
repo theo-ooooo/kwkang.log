fdc58f68310225cacb5382b196e562465ae50eea
Theo <73323028+kkwondev@users.noreply.github.com>
2024-11-26 15:12:56 +0900

    add: error page

---
+++ src/app/error.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+export default function error() {
+  const router = useRouter();
+  return (
+    <div className='flex py-3 flex-col gap-4 justify-center items-center w-full h-svh'>
+      <h2 className='text-5xl font-semibold text-red-500'>ERROR</h2>
+      {/* <p className='text-base'>페이지의 오류가 발생하였습니다.</p> */}
+      <button className='text-sm  p-2' onClick={() => router.push("/")}>
+        메인으로 돌아가기
+      </button>
+    </div>
+  );
+}
