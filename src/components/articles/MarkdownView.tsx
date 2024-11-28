dcb5bfb5c0ddefd53bf881096d1e0234e8258eda
Theo <73323028+kkwondev@users.noreply.github.com>
2024-11-28 15:44:55 +0900

    add: markdwon id add

--- src/components/articles/MarkdownView.tsx
+++ src/components/articles/MarkdownView.tsx
@@ -4,12 +4,16 @@ import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { dracula } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import slug from "rehype-slug";
+import raw from "rehype-raw";
+import { PluggableList } from "unified";
 
 export default function MarkdownView({ contentHtml }: { contentHtml: string }) {
   return (
     <div className='!w-full !max-w-[768px] prose dark:prose-invert'>
       <ReactMarkdown
         remarkPlugins={[remarkGfm]}
+        rehypePlugins={[raw, slug] as PluggableList}
         components={{
           code({ className, children, ...rest }) {
             const match = /language-(\w+)/.exec(className || "");
